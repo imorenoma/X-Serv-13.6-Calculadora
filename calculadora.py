720917d7d639566a0a7561c7371486ede4c3860a
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-02-10 17:38:32 +0100

    change lines between 27 to 36

--- calculadora.py
+++ calculadora.py
@@ -16,22 +16,24 @@ def f_res(num1, num2):
     print("your res is: ", float(num1)-float(num2))
 
 
+def f_mult(num1, num2):
+    print("your mult is: ", float(num1)*float(num2))
+
+
 def f_div(num1, num2):
-    try:
-        print("your sum is: ", float(num1) / float(num2))
-    except ZeroDivisionError:
-        print("you try to divided by zero")
+    print("your div is: ", float(num1)/float(num2))
 
 
 try:
 
-    if sys.argv[2] == '+':
-        f_sum(sys.argv[1], sys.argv[3])
-    elif sys.argv[2] == '-':
-        f_res(sys.argv[1], sys.argv[3])
-    elif sys.argv[2] == '/':
-        f_div(sys.argv[1], sys.argv[3])
-
+    if sys.argv[1] == "suma":
+        f_sum(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == "resta":
+        f_res(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == "multiplica":
+        f_mult(sys.argv[2], sys.argv[3])
+    elif sys.argv[2] == "divide":
+        f_div(sys.argv[2], sys.argv[3])
 
 except ValueError:
     print("could not convert string to float:", sys.argv[1], sys.argv[2], sys.argv[3])
@@ -39,8 +41,3 @@ except TypeError:
     print("Upss something is broken", sys.argv[1], sys.argv[2], sys.argv[3])
 except SyntaxError:
     print("Are tou sure:", sys.argv[1], sys.argv[2], sys.argv[3])
-
-
-
-
-

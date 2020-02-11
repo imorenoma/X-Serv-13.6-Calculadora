c5db344bd4a80fcaf03d2d14ea501194a72b9805
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-02-11 09:57:34 +0100

    new file cal2.py, calculator with a dictionary

---
+++ calc2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+"""
+CALCULADORA SIMPLE CON PYTHON VERSION 2
+"""
+
+if(len(sys.argv)!= 4):
+    print("Usage Error: $calc2.py OPERATION Number1 Number2")
+else:
+
+    try:
+        operation ={"suma": float(sys.argv[2])+float(sys.argv[3]),
+                    "resta": float(sys.argv[2])-float(sys.argv[3]),
+                    "multiplica": float(sys.argv[2])*float(sys.argv[3]),
+                    "divide": float(sys.argv[2])/float(sys.argv[3])}
+
+        print(operation.__getitem__(sys.argv[1]))
+    except ZeroDivisionError:
+        print("Usage Error: You try to divide by zero")
+    except ValueError:
+        print("could not convert string to float:", sys.argv[1], sys.argv[2], sys.argv[3])
